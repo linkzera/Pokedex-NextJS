15f092aa66ae666750533f495964ae57ae53b03e
Douglas <douglas04041996@hotmail.com>
2021-12-27 15:33:57 -0300

    getStaticProps fixado

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -4,20 +4,21 @@ import Layout from "../components/Layout";
 import Titulo from "../components/Titulo";
 
 export default function Index() {
-  const [name, setName] = useState('')
-  const handleName = (e) =>{
-    setName(e.target.value)
-  }
-  useEffect(() => {
-    console.log(name)
-    
-  }, [name])
+  const [name, setName] = useState("");
+  const handleName = (e) => {
+    setName(e.target.value);
+  };
 
   return (
     <div>
       <Titulo>Pokédex Next</Titulo>
       <Cabecalho titulo="Cabeçalho" />
-      <input type="text" defaultValue={""} onChange={handleName} placeholder="pokémon name"/>
+      <input
+        type="text"
+        defaultValue={""}
+        onChange={handleName}
+        placeholder="pokémon name"
+      />
       <Layout href={`/pokemon/${name || "1"}`}>Search Pokémon</Layout>
     </div>
   );
--- src/pages/pokemon/[name].jsx
+++ src/pages/pokemon/[name].jsx
@@ -1,30 +1,31 @@
-import { useRouter } from "next/router";
-import { useEffect, useState } from "react";
 import Cabecalho from "../../components/Cabecalho";
 import Layout from "../../components/Layout";
 import Titulo from "../../components/Titulo";
 
-export default function PokemonName() {
-  const [pokemon, setPokemon] = useState({});
-  const router = useRouter();
-  const name = router.query.name
-  useEffect(() => {
-    const getData = async ()=>{
-      const data = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`).then(res=>res.json())
-      console.log(data)
-     setPokemon(data)
-    }
-   getData()
- 
-  }, [name])
-
- 
+export default function PokemonName({ pokemon }) {
   return (
     <>
-    <Titulo>Pokémon {pokemon.name}</Titulo>
+      <Titulo>Pokémon {pokemon.name}</Titulo>
       <Cabecalho titulo={`pokemon ${pokemon.name}`} />
       <span>{pokemon.name}</span>
       <Layout />
     </>
   );
 }
+
+export const getStaticProps = async (context) => {
+  const { name } = context.params;
+
+  const data = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`).then(
+    (res) => res.json()
+  );
+
+  return { props: { pokemon: data } };
+};
+
+export const getStaticPaths = async () => {
+  return {
+    paths: [`/pokemon/1`, `/pokemon/bulbasaur`],
+    fallback: true,
+  };
+};
